1ae335627d4f20626f5bc4bebfde38eacc227dc7
Jacob <jstein@pivotal.io>
2019-09-03 11:48:13 -0500

    Finished CF app configuration lab

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,39 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private String port;
+    private String memoryLimit;
+    private String cfInstanceIndex;
+    private String cfInstanceAddr;
+
+    public EnvController(
+            @Value("${port:NOT SET}") String port,
+            @Value("${memory.limit:NOT SET}") String memoryLimit,
+            @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
+            @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddr
+    ) {
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceAddr = cfInstanceAddr;
+        this.cfInstanceIndex = cfInstanceIndex;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        Map<String, String> envMap = new HashMap<>();
+        envMap.put("PORT", this.port);
+        envMap.put("MEMORY_LIMIT", this.memoryLimit);
+        envMap.put("CF_INSTANCE_INDEX", this.cfInstanceIndex);
+        envMap.put("CF_INSTANCE_ADDR", this.cfInstanceAddr);
+        return envMap;
+    }
+}
